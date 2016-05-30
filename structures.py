62faec4031ee05abf3d3ce332b6a50c93a12803e
Jake Rachleff <jakerach@stanford.edu>
2016-05-30 00:46:43 -0700

    working AC data structure

--- structures.py
+++ structures.py
@@ -1,39 +1,50 @@
+from collections import deque
+
+
 NO_DIFFERENCE_SET = -1
 
 class Node(object):
-	def __init__(self, character):
+	def __init__(self, character, depth, parent):
 		self.character = character
+		self.depth = depth
 		self.word = None
+		self.parent = parent
+		self.ACsuffix_link = None
 		self.children = {}
+
+class ACNode(Node):
+	def __init__(self, character, depth, parent):
+		Node.__init__(self, character, depth, parent)
 		self.output_link = None
 		self.suffix_link = None
 
-class ACNode(Node):
-	def __init__(self, character):
-		Node.__init__(self, character)
-
 
 class CWNode(Node):
-	def __init__(self, character, depth):
-		Node.__init__(self, character)
-		self.depth = depth
+	def __init__(self, character, depth, parent):
+		Node.__init__(self, character, depth, parent)
 		self.min_difference = NO_DIFFERENCE_SET
+		self.output_link = None
+		self.CWsuffix_link = None
 
 class Trie(object):
-	def create_node(self, character):
-		return Node(character)
+	def create_node(self, character, depth, parent):
+		return Node(character, depth, parent)
 
 	def __init__(self):
 		self.size = 0
-		self.root = self.create_node(None)	#char doesn't exist for root
+		self.root = self.create_node(None, 0, None)	#char doesn't exist for root
 
 	def add_word(self, word):
 		current_node = self.root
+		current_depth = 1
 		for character in word:
 			next_node = current_node.children.get(character)
 			if not next_node:
-				next_node = self.create_node(character)
+				next_node = self.create_node(character, current_depth, current_node)
 				current_node.children[character] = next_node
+
+			if not next_node.depth == current_depth:
+				print ("ERROR: Incorrect depths")
 
 			current_node = next_node
 
@@ -56,13 +67,95 @@ class Trie(object):
 
 		if not (current_node.word == word):
 			print ("You suck at implementing tries")
-
 		return True
+
+	def is_root(self, node):
+		return node.character is None
+
+	def node_has_child(self, node, character):
+		return node.children.get(character) is not None
+
+	def get_AC_suffix_link(self, node):
+		searcher = node.parent.ACsuffix_link
+		while (not self.is_root(searcher)) and (not self.node_has_child(searcher, node.character)):
+			searcher = searcher.ACsuffix_link
+			if not searcher:
+				import pdb; pdb.set_trace()
+
+		if (self.node_has_child(searcher, node.character)):
+			return searcher.children[node.character]
+		else:
+			if (not self.is_root(searcher)):
+				print ("ERROR: Incorrect looping in suffix links")
+			return searcher
+
 
 
 class ACAuto(Trie):
-	def create_node(self, character):
-		return ACNode(character)
+	def create_node(self, character, depth, parent):
+		return ACNode(character, depth, parent)
+
+	def create_failure_links(self):
+		"""
+		Creates all failure links for an Aho Corsick Automata, should run in O(n) time
+		where n is the length of all strings in the automata put together. ONLY RUN THIS
+		ONCE ALL WORDS ARE INCLUDED.
+		"""
+		bfs_queue = deque()
+
+		# First, set suffix links for first children to root
+		for key in self.root.children:
+			child = self.root.children[key]
+			child.ACsuffix_link = self.root
+
+			for key2 in child.children:
+				grandchild = child.children[key2]
+				bfs_queue.append(grandchild)
+
+		while (len(bfs_queue) > 0):
+			current_node = bfs_queue.popleft();
+			for key in current_node.children:
+				child = current_node.children[key]
+				bfs_queue.append(child)
+
+			current_node.ACsuffix_link = self.get_AC_suffix_link(current_node)
+			suffix_is_word = current_node.ACsuffix_link.word is not None
+			current_node.output_link = current_node.ACsuffix_link if suffix_is_word else current_node.ACsuffix_link.output_link
+
+	def report_all_matches(self, text):
+		matches = deque()
+		pos = 0
+		current_node = self.root
+		for character in text:
+			# If our current node has character as a child, go to the next node
+			if self.node_has_child(current_node, character):
+				current_node = current_node.children[character]
+			else:
+				while not self.is_root(current_node):
+					current_node = current_node.ACsuffix_link
+					if self.node_has_child(current_node, character):
+						current_node = current_node.children[character]
+						break
+
+			if (current_node.word is not None):
+				matches.append((current_node.word, pos - len(current_node.word) + 1))
+
+			output_searcher = current_node.output_link
+			while (output_searcher is not None):
+				matches.append((output_searcher.word, pos - len(output_searcher.word) + 1))
+				output_searcher = output_searcher.output_link
+
+			pos += 1
+
+		for match, pos in matches:
+			print ("matched with " + match + " at position " + str(pos))
+
+
+
+
+
+
+
 
 
 
--- structures_test.py
+++ structures_test.py
@@ -1,6 +1,8 @@
 from structures import Trie, ACAuto
 
+
 def test_Trie():
+	print("TESTING THE TRIE DATA STRUCTURE")
 	our_trie = Trie()
 	our_trie.add_word("hello")
 	our_trie.add_word("help")
@@ -9,21 +11,30 @@ def test_Trie():
 	our_trie.add_word("hello")
 	our_trie.lookup("helpo")
 	our_trie.lookup("hello")
+	our_trie.lookup("help")
 	our_trie.lookup("howdy")
+	print("DONE TESTING THE TRIE DATA STRUCTURE\n\n\n")
 
 def test_ACAuto():
+	print("TESTING THE AHO CORSICK DATA STRUCTURE")
 	our_ACAuto = ACAuto()
 	print("First testing its trie structure: ")
 	our_ACAuto.add_word("hello")
 	our_ACAuto.add_word("help")
 	our_ACAuto.add_word("howdy")
 	our_ACAuto.add_word("james")
+	our_ACAuto.add_word("jake")
+	our_ACAuto.add_word("ello")
 	our_ACAuto.add_word("hello")
+	our_ACAuto.add_word("shello")
 	our_ACAuto.lookup("helpo")
 	our_ACAuto.lookup("hello")
 	our_ACAuto.lookup("howdy")
 
-	print("Now, testing its ACAuto ability")
+	print("\n\nNow, testing its ACAuto ability:")
+	our_ACAuto.create_failure_links()
+	our_ACAuto.report_all_matches("jameshelloa;sldkfj;asdljfadello")
+	print("DONE TESTING THE AHO CORSICK DATA STRUCTURE\n\n")
 
 def main():
 	test_Trie()
